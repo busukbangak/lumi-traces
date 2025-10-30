7a0cd2d27925149afa384c744b33332270f457a4
awanali <aliim.wanali@gmail.com>
2025-10-30 23:20:27 +0100

    feat: add new traceType to traces

--- backend/src/models/traces.ts
+++ backend/src/models/traces.ts
@@ -1,11 +1,25 @@
 import mongoose, { Schema } from 'mongoose';
 
+export enum TraceType {
+    Adventure = 'ADVENTURE',
+    Knowledge = 'KNOWLEDGE',
+}
+
+export enum TraceStatus {
+    ACTIVE = 'ACTIVE',
+    MISSING = 'MISSING',
+    PENDING = 'PENDING',
+    REMOVED = 'REMOVED',
+    INVALID = 'INVALID'
+}
+
 const traceSchema = new mongoose.Schema({
-    status: { type: String, enum: ['ACTIVE', 'MISSING', 'PENDING', 'REMOVED', 'INVALID'], required: true }, // Status field
+    status: { type: String, enum: Object.values(TraceStatus), required: true }, // Status field
     position: { type: [Number], required: true }, // Position as an array of numbers
     title: { type: String, required: true }, // Title field
     description: { type: String, required: true }, // Description field
     imageID: { type: Schema.Types.ObjectId, ref: 'images.files', required: true }, // Image field
+    traceType: { type: String, enum: Object.values(TraceType), required: true }, // Trace type field
 });
 
 export const Trace = mongoose.model('Trace', traceSchema);
